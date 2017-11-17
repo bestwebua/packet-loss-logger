db760bfeea789d52fbd27b7a3c9e62ea338c9173
Vladislav Trotsenko <admin@bestweb.com.ua>
2017-11-17 09:13:26 +0200

    .

--- pll.rb
+++ pll.rb
@@ -54,5 +54,6 @@ require 'net/ping'
         sleep 1
       break if time_current >= time_end
     end
-
-File.open(log, 'a+') { |data| data.puts "#### Ping to [#{host}], started at: #{time_start}, finished at: #{time_end}. Total timeouts: #{total_fails}. The worst time is #{the_worst_time} ms. ####" }
+  
+  File.open(log, 'a+') { |data| data.puts "#### Ping to [#{host}], started at: #{time_start}, finished at: #{time_end}. Total timeouts: #{total_fails}. The worst time is #{the_worst_time} ms. ####" }
+puts "#{IO.readlines(log).last[0..-2]}"
